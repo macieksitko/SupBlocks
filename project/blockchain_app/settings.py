6687ac265b8ba6e95f8a3aa050142ff3603dcfd0
macieksitko <mmacieksitko@gmail.com>
2020-12-24 11:54:42 +0100

    -

--- project/blockchain_app/settings.py
+++ project/blockchain_app/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Add these new lines
-#STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'),)
 
 
 
