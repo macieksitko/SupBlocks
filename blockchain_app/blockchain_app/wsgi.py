20beec5d4f6c23be6da108bf8102ec9263c51a88
macieksitko <mmacieksitko@gmail.com>
2020-12-24 13:19:58 +0100

    -

--- blockchain_app/blockchain_app/wsgi.py
+++ blockchain_app/blockchain_app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blockchain_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blockchain_app.blockchain_app.settings')
 
 application = get_wsgi_application()
